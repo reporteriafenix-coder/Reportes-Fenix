66fd6176bfc29bc308339b7ec4960bc7dd44eb21
reporteriafenix-coder <reporteriafenix@gmail.com>
2025-11-05 16:32:58 -0300

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,19 @@
+// usuarios.js
+const usuarios = [
+  { usuario: "admin", password: "Martel147", rol: "admin" },
+  { usuario: "raguayo@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "mpena@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "ggonzalez@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "garanda@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "agarcia@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "derlys.almiron@martel.com.py", password: "Martel147", rol: "Gerencia" },
+  { usuario: "jsabas@martel.com.py", password: "Martel147", rol: "B2C" },
+  { usuario: "liz.quintana@martel.com.py", password: "Martel147", rol: "B2B" },
+  { usuario: "ecomuser", password: "1234", rol: "ecom" },
+  { usuario: "Tienda_retail", password: "1234", rol: "tienda_retail" },
+  { usuario: "mgreen@martel.com.py", password: "Martel008", rol: "B2C" }
+];
+
+
+
+
