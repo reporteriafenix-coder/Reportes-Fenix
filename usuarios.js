fef606ea934612b851649c72108d1d1d4e853911
reporteriafenix-coder <reporteriafenix@gmail.com>
2025-11-18 15:40:07 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -14,8 +14,10 @@ const usuarios = [
   { usuario: "Tienda_retail", password: "1234", rol: "tienda_retail" },
   { usuario: "mgreen@martel.com.py", password: "Martel147", rol: "B2C" },
   { usuario: "tatianna.echeverria@enerbike.com.py", password: "Martel147", rol: "Gerencia" },
-    { usuario: "natalia.gadea@martel.com.py", password: "Martel147", rol: "admin" }
+  { usuario: "natalia.gadea@martel.com.py", password: "Martel147", rol: "admin" },
+  { usuario: "Tiendamcal", password: "Martelmcal", rol: "admin" }
 ];
+
 
 
 
